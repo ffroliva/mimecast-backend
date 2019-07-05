11ff12a254e45268b7782a862acf92ebf3cc18dd
Flavio Oliva <flavio.oliva@castgroup.com.br>
2019-07-05 14:30:37 -0300

    Creating API for mimecast app

---
+++ src/main/java/br/com/ffroliva/mimecast/MimecastApplication.java
@@ -0,0 +1,13 @@
+package br.com.ffroliva.mimecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimecastApplication.class, args);
+	}
+
+}
