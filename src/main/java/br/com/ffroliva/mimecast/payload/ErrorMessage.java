d7f4d36dfa213ee1637192b27ff80634def5892e
Flavio Oliva <flavio.oliva@castgroup.com.br>
2019-07-22 18:01:45 -0300

    Removing unused classe.

--- src/main/java/br/com/ffroliva/mimecast/payload/ErrorMessage.java
+++ src/main/java/br/com/ffroliva/mimecast/payload/ErrorMessage.java
@@ -1,22 +1,13 @@
 package br.com.ffroliva.mimecast.payload;
 
-import br.com.ffroliva.mimecast.config.properties.IMessageProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ErrorMessage {
 
-    private String error;
     private String message;
-
-    public ErrorMessage(IMessageProperty messageProperty) {
-        this.error = messageProperty.key();
-        this.message = messageProperty.message();
-    }
-
-    public ErrorMessage(String error, String message) {
-        this.error = error;
-        this.message = message;
-    }
+    private String status;
 
 }
